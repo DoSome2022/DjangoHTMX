4e042e9eb64c88781e5256126e58f28e91edd33f
DoSome2022 <DoSomeWork2022@gmail.com>
2023-06-09 17:01:52 +0800

    part12

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -13,26 +13,38 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.contrib import admin
+# from django.urls import path ,include
+# from django.contrib.auth import views
+# from cart.views import add_to_cart , cart , checkout
+
+# from core.views import shop , signup 
+# from product.views import product
+
+
+# urlpatterns = [
+#     path('', include('core.urls')),
+#     path('signup/', signup, name='signup'),
+#     path('logout/', views.LogoutView.as_view(), name='logout'),
+#     path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
+#     path('shop/', shop ,name="shop"),
+#     path('shop/<slug:slug>', product , name="product"),
+#     path('cart/', cart, name='cart'),
+#     path('cart/checkout/', checkout, name='checkout'),
+#     path('add_to_cart/<int:product_id>/',add_to_cart,name='add_to_cart'),
+#     path('admin/', admin.site.urls),
+# ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path ,include
-from django.contrib.auth import views
-from cart.views import add_to_cart , cart , checkout
-
-from core.views import shop , signup 
-from product.views import product
-
+from django.urls import path, include
 
 urlpatterns = [
     path('', include('core.urls')),
-    path('signup/', signup, name='signup'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
-    path('shop/', shop ,name="shop"),
-    path('shop/<slug:slug>', product , name="product"),
-    path('cart/', cart, name='cart'),
-    path('cart/checkout/', checkout, name='checkout'),
-    path('add_to_cart/<int:product_id>/',add_to_cart,name='add_to_cart'),
+    path('cart/', include('cart.urls')),
     path('admin/', admin.site.urls),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
