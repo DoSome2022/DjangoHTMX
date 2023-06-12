a7ad9be1cc27d4c7badf910dd2452a63588625ae
DoSome2022 <DoSomeWork2022@gmail.com>
2023-06-12 12:08:44 +0800

    part13

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from .cart import Cart
@@ -10,7 +11,9 @@ def add_to_cart(request, product_id):
     return render(request, 'cart/menu_cart.html')
 
 def cart(request):
+    
     return render(request, 'cart/cart.html')
 
+@login_required
 def checkout(request):
     return render(request, 'cart/checkout.html')
